947d0510f1808f4fffcc8d31ac8de1a63c5abb3c
Dean <zmes50416@gmail.com>
2015-05-21 00:52:01 +0800

    Add BBC news populator

---
+++ src/main/tw/edu/ncu/CJ102/CoreProcess/BBCNewsPopulator.java
@@ -0,0 +1,123 @@
+package tw.edu.ncu.CJ102.CoreProcess;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import tw.edu.ncu.CJ102.SettingManager;
+
+public class BBCNewsPopulator implements ExperimentFilePopulater {
+
+	private Path trainFileDir;
+	private Path testFileDir;
+	
+	protected Set<String> trainTopics;
+	protected Set<String> testTopics;
+	public final String[] TOPICS = {
+			"sport", "bussiness", "entertainment" , "politics" , "tech"
+	};
+	public final String DEFAULT_TOPIC_PATH = SettingManager.getSetting("bbcDataSet");
+	private TrainingTools trainerTom = new TrainingTools();
+	protected Path topicPath;
+	protected int trainSize = 0;
+	protected int testSize = 0;
+
+
+	public BBCNewsPopulator(Path projectDir,Path topicPath) {
+		this.trainFileDir = projectDir.resolve(this.TRAININGPATH);
+		this.testFileDir = projectDir.resolve(this.TESTINGPATH);
+		this.topicPath = topicPath;
+		trainTopics = new HashSet<>();
+		testTopics = new HashSet<>();
+	}
+	
+	public boolean addTrainingTopics(String topic){
+		return this.trainTopics.add(topic);
+	}
+	public boolean addTestingTopics(String topic){
+		return this.testTopics.add(topic);
+	}
+	
+	/**
+	 * @return the testSize
+	 */
+	public int getTestSize() {
+		return testSize;
+	}
+
+	/**
+	 * @param testSize the testSize to set
+	 */
+	public void setTestSize(int testSize) {
+		this.testSize = testSize;
+	}
+
+	/**
+	 * @return the trainSize
+	 */
+	public int getTrainSize() {
+		return trainSize;
+	}
+
+	/**
+	 * @param trainSize the trainSize to set
+	 */
+	public void setTrainSize(int trainSize) {
+		this.trainSize = trainSize;
+	}
+
+	/**
+	 * inject populate rule into the algorithm
+	 * Default to generate simple one
+	 */
+	public void setGenarationRule(){
+		
+	}
+	@Override
+	public boolean populateExperiment(int days) {
+		try {
+
+			for (int theDay = 1; theDay <= days; theDay++) {
+				Path todayTrainDir = this.trainFileDir.resolve("Day_" + theDay);
+				Files.createDirectories(todayTrainDir);
+				Path todayTestDir = this.testFileDir.resolve("Day_" + theDay);
+				Files.createDirectories(todayTestDir);
+				this.setGenarationRule();
+				for (String topic : trainTopics) {
+					trainerTom.point_topic_doc_generateSet(
+							topicPath.toString(), todayTrainDir.toString(),
+							topic, trainSize, theDay);
+				}
+				for (String topic : testTopics) {
+					trainerTom.point_topic_doc_generateSet(
+							topicPath.toString(), todayTestDir.toString(),
+							topic, testSize, theDay);
+				}
+
+			}
+			return true;
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	@Override
+	public String identifyTopic(File document) {//Should have the topic name before file ex:sport_01.txt
+		String fileName = document.getName();
+		String topicName = fileName.split("_")[0];
+		for(String name:TOPICS){
+			if(topicName.equals(name)){
+				return topicName;
+			}
+		}
+		
+		throw new IllegalArgumentException("can't find topic name patten in file: "+document.getName());
+	}
+
+}
---
+++ src/main/tw/edu/ncu/CJ102/CoreProcess/BBCNewsPopulatorTest.java
@@ -0,0 +1,73 @@
+package tw.edu.ncu.CJ102.CoreProcess;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BBCNewsPopulatorTest {
+	BBCNewsPopulator testSubject;
+	Path project;
+	@Before
+	public void setUp() throws Exception {
+		project = Files.createTempDirectory("BBCNews_");
+		Path topicPlace = new File("bbc").toPath();
+		testSubject = new BBCNewsPopulator(project,topicPlace);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		FileUtils.deleteDirectory(project.toFile());
+	}
+
+	@Test
+	public void testPopulateExperiment() {
+		int day=1;
+		testSubject.setTrainSize(1);
+		testSubject.setTestSize(1);
+		testSubject.addTrainingTopics("tech");
+		testSubject.addTestingTopics("tech");
+		testSubject.populateExperiment(day);
+		
+		Path traingDir = project.resolve(ExperimentFilePopulater.TRAININGPATH);
+		File testingDir = project.resolve(ExperimentFilePopulater.TESTINGPATH).toFile();
+		assertTrue(" should have create the training directory",Files.isDirectory(traingDir));
+		assertTrue(" should have create the testing directory",Files.isDirectory(testingDir.toPath()));
+		
+		assertEquals("Should have create train days dir",traingDir.toFile().list().length,day);
+		assertEquals("Should have create train days dir",testingDir.list().length,day);
+		for(File dayDir:testingDir.listFiles()){
+			assertEquals("Should have generate right number of file",dayDir.list().length,1);
+			for(File file:dayDir.listFiles()){
+				String topic = testSubject.identifyTopic(file);
+				File topicDir = testSubject.topicPath.resolve(topic).toFile();
+				List<String> topicList = Arrays.asList(topicDir.list());
+				assertTrue("Should have the same File",topicList.contains(file.getName()));
+			}
+		}
+		
+	}
+
+	@Test
+	public void testIdentifyTopic() {
+			File dummyFile = new File("sport_(1).txt");
+			String topic = this.testSubject.identifyTopic(dummyFile);
+			assertEquals("should get the right topic name","sport", topic);
+			
+	}
+
+}
